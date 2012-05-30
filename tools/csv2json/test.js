248a36ec12cc716d86565676c3e3cb172bad51e6
Konstantin Root <konstantin.root@windowslive.com>
2012-05-30 23:11:50 +0300

    Small test to parse csv in JS

---
+++ tools/csv2json/test.js
@@ -0,0 +1,112 @@
+	// http://stackoverflow.com/questions/8493195/how-can-i-parse-a-csv-string-with-javascript
+
+	// This will parse a delimited string into an array of
+    // arrays. The default delimiter is the comma, but this
+    // can be overriden in the second argument.
+    function CSVToArray( strData, strDelimiter ){
+        // Check to see if the delimiter is defined. If not,
+        // then default to comma.
+        strDelimiter = (strDelimiter || ",");
+
+        // Create a regular expression to parse the CSV values.
+        var objPattern = new RegExp(
+                (
+                        // Delimiters.
+                        "(\\" + strDelimiter + "|\\r?\\n|\\r|^)" +
+
+                        // Quoted fields.
+                        "(?:\"([^\"]*(?:\"\"[^\"]*)*)\"|" +
+
+                        // Standard fields.
+                        "([^\"\\" + strDelimiter + "\\r\\n]*))"
+                ),
+                "gi"
+                );
+
+
+        // Create an array to hold our data. Give the array
+        // a default empty first row.
+        var arrData = [[]];
+
+        // Create an array to hold our individual pattern
+        // matching groups.
+        var arrMatches = null;
+
+
+        // Keep looping over the regular expression matches
+        // until we can no longer find a match.
+        while (arrMatches = objPattern.exec( strData )){
+
+                // Get the delimiter that was found.
+                var strMatchedDelimiter = arrMatches[ 1 ];
+
+                // Check to see if the given delimiter has a length
+                // (is not the start of string) and if it matches
+                // field delimiter. If id does not, then we know
+                // that this delimiter is a row delimiter.
+                if (
+                        strMatchedDelimiter.length &&
+                        (strMatchedDelimiter != strDelimiter)
+                        ){
+
+                        // Since we have reached a new row of data,
+                        // add an empty row to our data array.
+                        arrData.push( [] );
+
+                }
+
+
+                // Now that we have our delimiter out of the way,
+                // let's check to see which kind of value we
+                // captured (quoted or unquoted).
+                if (arrMatches[ 2 ]){
+
+                        // We found a quoted value. When we capture
+                        // this value, unescape any double quotes.
+                        var strMatchedValue = arrMatches[ 2 ].replace(
+                                new RegExp( "\"\"", "g" ),
+                                "\""
+                                );
+
+                } else {
+
+                        // We found a non-quoted value.
+                        var strMatchedValue = arrMatches[ 3 ];
+
+                }
+
+
+                // Now that we have our value string, let's add
+                // it to the data array.
+                arrData[ arrData.length - 1 ].push( strMatchedValue );
+        }
+
+        // Return the parsed data.
+        return( arrData );
+    }
+	
+var Strings = {
+        /**
+         * Wrapped csv line parser
+         * @param s string delimited csv string
+         * @param sep separator override
+         * @attribution : http://www.greywyvern.com/?post=258 (comments closed on blog :( )
+         */
+        parseCSV : function(s,sep) {
+            // http://stackoverflow.com/questions/1155678/javascript-string-newline-character
+            var universalNewline = /\r\n|\r|\n/g;
+            var a = s.split(universalNewline);
+            for(var i in a){
+                for (var f = a[i].split(sep = sep || ","), x = f.length - 1, tl; x >= 0; x--) {
+                    if (f[x].replace(/"\s+$/, '"').charAt(f[x].length - 1) == '"') {
+                        if ((tl = f[x].replace(/^\s+"/, '"')).length > 1 && tl.charAt(0) == '"') {
+                            f[x] = f[x].replace(/^\s*"|"\s*$/g, '').replace(/""/g, '"');
+                          } else if (x) {
+                        f.splice(x - 1, 2, [f[x - 1], f[x]].join(sep));
+                      } else f = f.shift().split(sep).concat(f);
+                    } else f[x].replace(/""/g, '"');
+                  } a[i] = f;
+        }
+        return a;
+        }
+    }
