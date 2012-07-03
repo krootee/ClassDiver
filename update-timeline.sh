b59baa78795d49f565bb560954707bee0b8cb658
Konstantin Root <konstantin.root@windowslive.com>
2012-07-03 22:42:05 +0300

    removed /font directory from Timeline control - it seems that its not used.

--- update-timeline.sh
+++ update-timeline.sh
@@ -1,7 +1,7 @@
 echo Copying updated compiled files from Timeline project to /webroot directory
 cp ../Timeline/compiled/css/timeline.* webroot/timeline/css
 cp ../Timeline/compiled/css/themes/timeline-texture.png webroot/timeline/css/themes
-cp ../Timeline/compiled/css/themes/font/*.* webroot/timeline/css/themes/font
+#cp ../Timeline/compiled/css/themes/font/*.* webroot/timeline/css/themes/font
 cp ../Timeline/compiled/js/timeline*.* webroot/timeline/js
 cp ../Timeline/compiled/js/locale/en.js webroot/timeline/js/locale
 cp ../Timeline/compiled/lib/jquery-min.js webroot/timeline/lib
