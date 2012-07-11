8d2780f5403bb38a5db99b28fdb49c4881a13bf4
Aleksandr Petuhhov <apetuhhov@gmail.com>
2012-07-11 09:49:40 +0300

    bouncing effect on title page made more realistic

--- webroot/js/index.js
+++ webroot/js/index.js
@@ -1,6 +1,10 @@
 $(function() {
 	$('#CD_text4').mouseenter(function() {
-		$('#planner').effect('bounce', {}, 500);
+		$('#planner').effect('bounce', {
+			distance : 10,
+			times : 3,
+			direction: 'right'
+		}, 400);
 	});
 	$('#CD_text4').click(function() {
 		window.location = 'calendar_full.html';
