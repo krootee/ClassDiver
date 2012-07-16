4e9e4f3f379ddb433bd95e8b757e6ef570083f13
Konstantin Root <konstantin.root@windowslive.com>
2012-07-16 13:47:26 +0300

    Timeline-min is now removed - minification will be done during deployment. Adjusted bash scripts to account for latest changes

--- update-timeline.sh
+++ update-timeline.sh
@@ -2,6 +2,6 @@ echo Copying updated compiled files from Timeline project to /webroot directory
 cp ../Timeline/compiled/css/timeline.* webroot/timeline/css
 cp ../Timeline/compiled/css/themes/timeline-texture.png webroot/timeline/css/themes
 #cp ../Timeline/compiled/css/themes/font/*.* webroot/timeline/css/themes/font
-cp ../Timeline/compiled/js/timeline*.* webroot/timeline/js
+cp ../Timeline/compiled/js/timeline.js webroot/timeline/js
+cp ../Timeline/compiled/js/timeline-embed.js webroot/timeline/js
 cp ../Timeline/compiled/js/locale/en.js webroot/timeline/js/locale
-#cp ../Timeline/compiled/lib/jquery-min.js webroot/timeline/lib
